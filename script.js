0053ca18ed188361b1334b3492fc663513260f26
meghanareddy1808 <108571707+meghanareddy1808@users.noreply.github.com>
2022-12-20 19:48:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
-let parentDiv=document.querySelector(".main")
-let input1=document.querySelector("#num1");
-let input2=document.querySelector("#num2");
-let sum=document.querySelector(".sum");
-let difference=document.querySelector(".difference");
-let product=document.querySelector(".product");
-let division=document.querySelector(".division");
-let result=document.querySelector(".result");
-sum.addEventListener('click',()=>{
-    let n1=+input1.value;
-    let n2=+input2.value;
-    let total=n1+n2;
-    result.textContent=total;
+const apikey = "92fbccf377b70c2fcc34474bea5368f1";
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const url = (city) =>
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
 
-});
-difference.addEventListener('click',()=>{
-    n1=+input1.value;
-    n2=+input2.value
-    let total=n1-n2;
-    result.textContent=total;
-});
-product.addEventListener('click',()=>{
-    n1=+input1.value;
-    n2=+input2.value
-    let total=n1*n2;
-    result.textContent=total;
-});
-division.addEventListener('click',()=>{
-    n1=+input1.value;
-    n2=+input2.value
-    let total=n1/n2;
-    result.textContent=total;
-});
+async function getWeatherByLocation(city) {
+    const resp = await fetch(url(city), { origin: "cors" });
+    const respData = await resp.json();
+
+    console.log(respData);
+
+    addWeatherToPage(respData);
+}
+
+function addWeatherToPage(data) {
+    const temp = KtoC(data.main.temp);
+
+    const weather = document.createElement("div");
+    weather.classList.add("weather");
+
+    weather.innerHTML = `
+        <h2>Weather in ${data.name}</h2>
+       <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />${data.weather[0].main} <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
+       <h2>${temp}°C</h2>
+       
+        <h3>Humidity:${data.main.humidity}</h3>
+        
+    `;
+
+    // cleanup
+    main.innerHTML = "";
+
+    main.appendChild(weather);
+}
+
+function KtoC(K) {
+    return Math.floor(K - 273.15);
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const city = search.value;
+
+    if (city) {
+        getWeatherByLocation(city);
+    }
+});; 
